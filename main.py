2aa414f7301e65565d7d6ba764945cad710b4704
Inderjeet <94882715+Inderjeet3@users.noreply.github.com>
2025-07-02 21:52:01 -0400

    grouped and cleaned up data

--- main.py
+++ main.py
@@ -17,62 +17,70 @@ import matplotlib.pyplot as plt
 # Print every value to 3 decimal places
 pd.set_option('display.float_format', '{:.3f}'.format)
 
-# store the data from avocado.csv into the following variable
 avocadoDataFrame = pd.read_csv('avocado.csv', index_col=0)
-avocadoDataFrame['Date'] = pd.to_datetime(avocadoDataFrame['Date'])
 
-avocadoDataFrame['year'] = avocadoDataFrame['Date'].dt.year
+cleanData = ['year', 'AveragePrice', 'Total Volume', 'type', 'region']
 
-averagePricePerYearPerCity = avocadoDataFrame.groupby(['region', 'year'])['AveragePrice'].mean().unstack()
+# store the data from avocado.csv into the following variable
+avocadoDataFrame = avocadoDataFrame[cleanData].copy()
+#avocadoDataFrame['Date'] = pd.to_datetime(avocadoDataFrame['Date'])
 
-averageForEachRegionAllYears = averagePricePerYearPerCity.mean(axis=1)
+#avocadoDataFrame['year'] = avocadoDataFrame['Date'].dt.year
+
+averagePricePerYearPerCity = avocadoDataFrame.groupby(['region', 'year', 'type']).agg({'AveragePrice': 'mean', 'Total Volume': 'sum'}).reset_index()
+
+#averageForEachRegionAllYears = averagePricePerYearPerCity.mean(axis=1)
 
 print(averagePricePerYearPerCity)
 
-print("\nTotal Average \n")
+# albanymean = avocadoDataFrame[(avocadoDataFrame['region'] == 'Albany') & (avocadoDataFrame['year'] == 2015) & (avocadoDataFrame['type'] == 'conventional')]
+# result = albanymean['AveragePrice'].mean()
+# print("it issssssssssssss")
+# print(result)
+# print("\nTotal Average \n")
 
-dummy = averageForEachRegionAllYears.reset_index()
-dummy.columns = ('Region', 'Average Price')
+# dummy = averageForEachRegionAllYears.reset_index()
+# dummy.columns = ('Region', 'Average Price')
 
-midPoint = len(dummy) // 2 + len(dummy) % 2
-leftHalf = dummy.iloc[:midPoint].reset_index(drop=True)
-rightHalf = dummy.iloc[midPoint:].reset_index(drop=True)
+# midPoint = len(dummy) // 2 + len(dummy) % 2
+# leftHalf = dummy.iloc[:midPoint].reset_index(drop=True)
+# rightHalf = dummy.iloc[midPoint:].reset_index(drop=True)
 
-leftHalf.columns = ['Region', 'Average Price']
-rightHalf.columns = ['Region', 'Average Price']
+# leftHalf.columns = ['Region', 'Average Price']
+# rightHalf.columns = ['Region', 'Average Price']
 
-splitDisplay = pd.concat([leftHalf, rightHalf], axis=1)
-print(splitDisplay)
+# splitDisplay = pd.concat([leftHalf, rightHalf], axis=1)
+# print(splitDisplay)
 
-# print("Overview of columns and data: ")
-# avocadoDataFrame.info()
+# # print("Overview of columns and data: ")
+# # avocadoDataFrame.info()
 
-# print("Overview of stats for Date: ")
-# print(avocadoDataFrame['Date'].describe())
+# # print("Overview of stats for Date: ")
+# # print(avocadoDataFrame['Date'].describe())
 
-# print("Overview of stats for the Average Prices: ")
-# print(avocadoDataFrame['AveragePrice'].describe())
+# # print("Overview of stats for the Average Prices: ")
+# # print(avocadoDataFrame['AveragePrice'].describe())
 
-# print("Overview of stats for the Year: ")
-# print(avocadoDataFrame['year'].describe())
+# # print("Overview of stats for the Year: ")
+# # print(avocadoDataFrame['year'].describe())
 
-# avocadoDataFrame.plot()
+# # avocadoDataFrame.plot()
+# # plt.show()
+
+# for i in range(54):
+
+
+# axis = averagePricePerYearPerCity.T.plot(marker='o', figsize=(14,8))
+
+# axis.set_title("Average Avocado Price Per Year in Major US Cities")
+# axis.set_xlabel('Year')
+# axis.set_ylabel('Average Price')
+
+# axis.legend(title="Major US Cities", title_fontsize=12, fontsize=10, loc="upper left", ncol=2)
+
+# plt.grid(True)
+# plt.tight_layout()
+# plt.style.use('seaborn-v0_8-colorblind')
 # plt.show()
-
-for i in range(54):
-
-
-axis = averagePricePerYearPerCity.T.plot(marker='o', figsize=(14,8))
-
-axis.set_title("Average Avocado Price Per Year in Major US Cities")
-axis.set_xlabel('Year')
-axis.set_ylabel('Average Price')
-
-axis.legend(title="Major US Cities", title_fontsize=12, fontsize=10, loc="upper left", ncol=2)
-
-plt.grid(True)
-plt.tight_layout()
-plt.style.use('seaborn-v0_8-colorblind')
-plt.show()
 
 
