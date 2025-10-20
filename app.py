2edfa0572367f573262c5a5a34d49b9843bed12a
Inderjeet <94882715+Inderjeet3@users.noreply.github.com>
2025-10-19 20:46:49 -0400

    added more deployment logic in app.py

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ from fastapi import FastAPI # create web API's and define routes like /api/citie
 from fastapi.middleware.cors import CORSMiddleware # CORS (cross origin resourse sharing) allowes react (localhost3000) and backend (localhost:8000) to communicate
 import main # main class with data cleaning and forecasting logic
 
+from fastapi.staticfiles import StaticFiles # allows fastapi to serve static files
+from fastapi.responses import FileResponse # allows fastapi to return a single file (index.html) in response to a request
+import os # handles file paths for different os's
+
 # create an instance of thr FASTAPI application where app is the object and will handle all HTTP requests
 app = FastAPI()
+
+# get the directoyr name of wehre app.py is located, and also point to the frontend build folder
+appDirectory = os.path.join(os.path.dirname(__file__), "frontend", "build")
+
+# so that whenever someone requests /static, serve it frontend/build/static
+app.mount("/static", StaticFiles(directory=os.path.join(appDirectory, "static")), name="static")
 
 # add CORS middleware to the fastapi app and allow all origins, HTTP methods, and headers to make requests to this API ( change when in production '*' to more limited access)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True,allow_methods=["*"], allow_headers=["*"])
@@ -31,4 +41,11 @@ def forecast(city: str, avocadoType: str):
         "forecast": forecastData.to_dict(orient="records"),
         "seasonality": seasonalityData.to_dict(orient="records")
     }
+
+# when someone uses the root url "/" serve index.html
+app.get("/")
+app.get("/{full_path:path}")
+def reactApp(full_path: str = ""):
+    pathIndex = os.path.join(appDirectory, "index.html")
+    return FileResponse(pathIndex)
 
